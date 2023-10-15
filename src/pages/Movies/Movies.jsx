564e5d05fdcb22715c31c81820d3f3db309a262e
sofiialives <127687828+sofiialives@users.noreply.github.com>
2023-10-15 23:38:32 +0300

    nothing

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,5 +1,61 @@
-export const Movies = () =>{
-    return(
-        <input type="text" />
-    )
-}
+import { useEffect, useState } from 'react';
+import css from './Movies.module.css';
+import { useSearchParams, Link } from 'react-router-dom';
+import { DayList } from 'services/DayList';
+import { Loader } from 'components/Loader';
+import { Error } from 'components/Error';
+
+export const Movies = () => {
+  const [loading, setLoading] = useState(false);
+  const [links, setLinks] = useState(null);
+  const [error, setError] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query');
+
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchMoviesDetails = async () => {
+      try {
+        setLoading(true);
+        const movieInfo = await DayList(query);
+        setLinks(movieInfo.results);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchMoviesDetails();
+  }, [query]);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const searchingString = event.currentTarget.elements.searchMovie.value;
+    setSearchParams({ query: searchingString });
+  };
+  return (
+    <div className={css.div}>
+      <form onSubmit={handleSubmit}>
+        <label>
+            <p>Enter...</p>
+          <input type="text" name="searchMovie" required />
+        </label>
+        <button type="submit">Search</button>
+      </form>
+      <section>
+        {loading && <Loader />}
+        {error && <Error />}
+        {links !== null && (
+          <ul>
+            {links.map((link, i) => (
+              <li key={i}>
+                <Link to={`/movies/${link.id}`}>{link.title || link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </section>
+    </div>
+  );
+};
